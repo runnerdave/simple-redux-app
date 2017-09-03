f863d24a021487636c46b5f2890e8449e40f2f28
runnerdave <runnerdave@gmail.com>
2017-09-03 22:34:46 +1000

    started the book detail section

--- src/reducers/reducer_books.js
+++ src/reducers/reducer_books.js
@@ -1,6 +1,6 @@
 export default function () {
     return [
-        {title: 'Javacript: the good parts'},
+        {title: 'Javascript: the good parts'},
         {title: 'Harry Potter'},
         {title: 'The Dark Tower'},
         {title: 'Eloquent Ruby'}
